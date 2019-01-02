77e79878d05eabbc93058b7953e8b84ee9e79bc7
Ken Heglund <github@orderedbytes.com>
2019-01-01 18:23:14 -0800

    Updates OBWFilteringMenuFieldEditor for style.

--- Silversides/OBWFilteringMenu/Internal/OBWFilteringMenuFieldEditor.swift
+++ Silversides/OBWFilteringMenu/Internal/OBWFilteringMenuFieldEditor.swift
@@ -11,26 +11,28 @@ import Cocoa
 class OBWFilteringMenuFieldEditor: NSTextView {
     
     /*==========================================================================*/
-    override init( frame frameRect: NSRect, textContainer container: NSTextContainer? ) {
-        super.init( frame: frameRect, textContainer: container )
+    override init(frame frameRect: NSRect, textContainer container: NSTextContainer?) {
+        super.init(frame: frameRect, textContainer: container)
         self.isFieldEditor = true
     }
     
     /*==========================================================================*/
-    required init?( coder: NSCoder ) {
-        fatalError( "init(coder:) has not been implemented" )
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     /*==========================================================================*/
     // MARK: - NSTextView overrides
     
     /*==========================================================================*/
-    override func setSelectedRange( _ charRange: NSRange ) {
+    override func setSelectedRange(_ charRange: NSRange) {
         
-        let automaticSelectionChange = ( RunLoop.current.currentMode != nil )
+        let automaticSelectionChange = (RunLoop.current.currentMode != nil)
         
-        guard !automaticSelectionChange else { return }
+        guard automaticSelectionChange == false else {
+            return
+        }
         
-        super.setSelectedRange( charRange )
+        super.setSelectedRange(charRange)
     }
 }
